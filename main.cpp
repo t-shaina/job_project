524bf150c2d211fdf2ad62b48af90ff380b33500
Tanya <t-shaina@mail.ru>
2023-04-18 19:52:23 +0300

    Satart commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    w.setFixedSize(500,350);
+    Start_page start_page(&w);
+
+    return a.exec();
+}
---
+++ start_page.h
@@ -0,0 +1,29 @@
+#ifndef START_PAGE_H
+#define START_PAGE_H
+
+#include <QWidget>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+#include <QGridLayout>
+
+class Start_page : public QGroupBox
+{
+    Q_OBJECT
+public:
+    explicit Start_page(QWidget *parent = nullptr);
+    ~Start_page();
+private:
+    QLabel* label_email;
+    QLabel* label_password;
+    QLineEdit* edit_email;
+    QLineEdit* edit_password;
+    QPushButton* button_entry;
+    QGridLayout* layout_page1;
+
+public slots:
+    void on_button_entry_clicked();
+};
+
+#endif // START_PAGE_H
